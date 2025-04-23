2db2cb81e6a35bcc6af0ee5ca017eb3dfee9b760
llll12t <147978645+llll12t@users.noreply.github.com>
2025-04-23 23:36:21 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,3 @@
 // กำหนด API URL (URL นี้ใช้สำหรับ GET และ POST ทั้งคู่)
 const API_URL = "https://script.google.com/macros/s/AKfycbzJ1VvqlIg_vKgmLnzzsBqdaUPSDNUV4BEhqLjYAxamOqd_OHxR1wH8mfogLJL6T0Aa7A/exec";
+const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbzzh9QtkmwySAx7dk2f5brlMnsYPl8IY4WdQhev5s7pzuuDPvc3GxPa7bAempFu5r71Yw/exec';
