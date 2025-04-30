412b0efd11969cc28d9c9bb1966bbb2274934c30
llll12t <147978645+llll12t@users.noreply.github.com>
2025-04-30 10:32:28 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,9 @@
+
+// กำหนด LIFF ID (EVENT)
+LIFF_EVENT = "2007305761-pnRkn7MM"
+LIFF_HISTORY = "2007305761-pnRkn7MM"
+LIFF_ADMIN = "2007305761-pnRkn7MM"
+
 // กำหนด API URL (Admin)
 const DASBOARD_URL = "https://script.google.com/macros/s/AKfycbzJ1VvqlIg_vKgmLnzzsBqdaUPSDNUV4BEhqLjYAxamOqd_OHxR1wH8mfogLJL6T0Aa7A/exec";
 const MEMBER_URL = 'https://script.google.com/macros/s/AKfycbyTGVFsuEyk3RPaQDun0vq-MaTMpZIfdSin13CcneDQDh_h1x7D2Vv5DkxQgvvnf6nj/exec';
