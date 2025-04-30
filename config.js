e3516b664cef4913a168778f942dae7530bf5829
llll12t <147978645+llll12t@users.noreply.github.com>
2025-04-30 10:29:52 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,7 @@
-// กำหนด API URL (URL นี้ใช้สำหรับ GET และ POST ทั้งคู่)
-const API_URL = "https://script.google.com/macros/s/AKfycbzJ1VvqlIg_vKgmLnzzsBqdaUPSDNUV4BEhqLjYAxamOqd_OHxR1wH8mfogLJL6T0Aa7A/exec";
+// กำหนด API URL (Admin)
+const DASBOARD_URL = "https://script.google.com/macros/s/AKfycbzJ1VvqlIg_vKgmLnzzsBqdaUPSDNUV4BEhqLjYAxamOqd_OHxR1wH8mfogLJL6T0Aa7A/exec";
+const MEMBER_URL = 'https://script.google.com/macros/s/AKfycbyTGVFsuEyk3RPaQDun0vq-MaTMpZIfdSin13CcneDQDh_h1x7D2Vv5DkxQgvvnf6nj/exec';
+const EVENT_URL = 'https://script.google.com/macros/s/AKfycbwGDZurmYzm3Vw5lXB8Vl2x7ytx6WWvkpVg4699FDJOsTWPjKUg4Zs-eF2ELv0bUYUn/exec';
+
+// กำหนด API URL (user)
 const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbx0VHAjmzhOPP9D0ncf0ln5-wnLd_Ycy9MrAeCtdgRP4EMRD-Y7pQ_hCAFso2wHs3M7tg/exec';
