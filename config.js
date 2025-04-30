a835677fd0cfbc89136323f99fa0fc7996e5b933
llll12t <147978645+llll12t@users.noreply.github.com>
2025-04-30 14:07:57 +0700

    Add files via upload
    update API connect

--- config.js
+++ config.js
@@ -1,14 +1,12 @@
 
 // กำหนด LIFF ID (EVENT)
-LIFF_EVENT = "2007305761-pnRkn7MM"
-LIFF_HISTORY = "2007305761-pnRkn7MM"
-LIFF_ADMIN = "2007305761-pnRkn7MM"
+const LIFF_EVENT = '2007305761-opN68Y00'; // LIFF ID EVENT
+const LIFF_HISTORY = '2007305761-pnRkn7MM'; // LIFF ID HISTORY 
 
 // กำหนด API URL (Admin)
-const DASBOARD_URL = "https://script.google.com/macros/s/AKfycbzJ1VvqlIg_vKgmLnzzsBqdaUPSDNUV4BEhqLjYAxamOqd_OHxR1wH8mfogLJL6T0Aa7A/exec";
+const ADMIN_URL = "https://script.google.com/macros/s/AKfycbzJ1VvqlIg_vKgmLnzzsBqdaUPSDNUV4BEhqLjYAxamOqd_OHxR1wH8mfogLJL6T0Aa7A/exec";
 const MEMBER_URL = 'https://script.google.com/macros/s/AKfycbyTGVFsuEyk3RPaQDun0vq-MaTMpZIfdSin13CcneDQDh_h1x7D2Vv5DkxQgvvnf6nj/exec';
-const EVENT_URL = 'https://script.google.com/macros/s/AKfycbwGDZurmYzm3Vw5lXB8Vl2x7ytx6WWvkpVg4699FDJOsTWPjKUg4Zs-eF2ELv0bUYUn/exec';
+const ADDEVENT_URL = "https://script.google.com/macros/s/AKfycbwGDZurmYzm3Vw5lXB8Vl2x7ytx6WWvkpVg4699FDJOsTWPjKUg4Zs-eF2ELv0bUYUn/exec";
 
 // กำหนด API URL (user)
-const API_URL = "https://script.google.com/macros/s/AKfycbzJ1VvqlIg_vKgmLnzzsBqdaUPSDNUV4BEhqLjYAxamOqd_OHxR1wH8mfogLJL6T0Aa7A/exec";
 const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbx0VHAjmzhOPP9D0ncf0ln5-wnLd_Ycy9MrAeCtdgRP4EMRD-Y7pQ_hCAFso2wHs3M7tg/exec';
