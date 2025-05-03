00630ca577ac77bfc28077288096634c9373eded
llll12t <147978645+llll12t@users.noreply.github.com>
2025-05-04 00:58:52 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const LIFF_EVENT = '2007305761-opN68Y00'; // LIFF ID EVENT
 const LIFF_HISTORY = '2007305761-pnRkn7MM'; // LIFF ID HISTORY 
 
 // กำหนด API URL (Admin)
-const ADMIN_URL = "https://script.google.com/macros/s/AKfycbx0VHAjmzhOPP9D0ncf0ln5-wnLd_Ycy9MrAeCtdgRP4EMRD-Y7pQ_hCAFso2wHs3M7tg/exec";
+const ADMIN_URL = "https://script.google.com/macros/s/AKfycbwOiSYN37QTLiFdz8F0VfVBHGGsoA7Rn6DoAQn0ZBa7Ss6E-UP6WDZVncDgnVby1cVOyQ/exec";
 const MEMBER_URL = 'https://script.google.com/macros/s/AKfycbyTGVFsuEyk3RPaQDun0vq-MaTMpZIfdSin13CcneDQDh_h1x7D2Vv5DkxQgvvnf6nj/exec';
 const ADDEVENT_URL = "https://script.google.com/macros/s/AKfycbwGDZurmYzm3Vw5lXB8Vl2x7ytx6WWvkpVg4699FDJOsTWPjKUg4Zs-eF2ELv0bUYUn/exec";
 
